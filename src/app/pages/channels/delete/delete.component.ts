88aeaf95ad91a8f14995a364ce69bce666cec0ab
Alexandra Cantarano <alexandra.cantarano@gmail.com>
2022-12-21 22:45:03 +0100

    add CRUD Channel

--- src/app/pages/channels/delete/delete.component.ts
+++ src/app/pages/channels/delete/delete.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ChannelService } from '../../service/channel.service';
+
+const url = "http://localhost:8080/channel/delete";
 
 @Component({
   selector: 'app-delete',
   templateUrl: './delete.component.html',
   styleUrls: ['./delete.component.css']
 })
-export class DeleteComponent {
+export class DeleteComponent implements OnInit {
+
+  public id?: number;
+  public channel?: any;
+
+  constructor(
+    private channelService: ChannelService,
+    private route: ActivatedRoute,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.channelService.getChannelById(url, this.id);
+    this.channelService.getChannel.subscribe(data => this.channel = data);
+  }
+
+  proceedToDelete()
+  {
+    this.channelService.deleteChannel(url, this.id);
+    this.router.navigate(['/channel/list']);
+  }
 
 }
--- src/app/pages/channels/edit/edit.component.ts
+++ src/app/pages/channels/edit/edit.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ChannelService } from '../../service/channel.service';
+
+const url = "http://localhost:8080/channel/put";
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent {
+export class EditComponent implements OnInit {
+
+  public id?: number;
+  public form: FormGroup;
+
+  constructor(
+    private channelService: ChannelService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ){
+    this.form = this.formBuilder.group({
+      name: [],
+      description: [],
+      createdAt: [],
+      updatedAt: []
+    })
+  }
+
+  ngOnInit(): void {
+
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.channelService.getChannelById(url, this.id);
+    this.channelService.getChannel.subscribe(data => {
+
+       this.form.patchValue({
+        name: data.name,
+        description: data.description,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt
+       });
+
+    });
+  }
+
+  public updateChannel(): void {
+    this.channelService.editChannel(url, this.id, this.form.value);
+    // this.router.navigate(['/user/find', this.id]);
+    this.router.navigate(['/channel/list']);
+  }
 
 }
