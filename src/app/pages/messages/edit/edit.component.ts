223e66cd793b087a730689b6dc3b1c431c3a8cdc
Laura Dufaut <laura_dufaut@yahoo.fr>
2022-12-22 09:27:19 +0100

    add changes messages + icon + index

--- src/app/pages/messages/edit/edit.component.ts
+++ src/app/pages/messages/edit/edit.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from '../../service/message.service';
+
+const url = "http://localhost:8080/message/post";
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent {
+export class EditComponent implements OnInit {
+
+  public id?: number;
+  public form: FormGroup;
+
+  constructor(
+    private messageService: MessageService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private router: Router
+  ){
+    this.form = this.fb.group({
+      content: [],
+      createdAt: [],
+      updatedAt: []
+    })
+  }
+
+  ngOnInit(): void {
+   
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.messageService.getMessageById(url, this.id);
+    this.messageService.getMessage.subscribe(data => {
+      this.form.patchValue({
+        content: data.content,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt
+      });
+    });
+  }
+
+  public submitMessage(): void {
+    this.messageService.editMessage(url, this.id, this.form.value);
+    this.router.navigate(['/message/list']);
+  }
 
 }
--- src/app/pages/messages/show/show.component.ts
+++ src/app/pages/messages/show/show.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MessageService } from '../../service/message.service';
+
+const url = "http://localhost:8080/message/find";
 
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
   styleUrls: ['./show.component.css']
 })
-export class ShowComponent {
+export class ShowComponent implements OnInit {
+
+  public id?: number;
+  public message?: any;
+
+  constructor(
+    private messageService: MessageService,
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.messageService.getMessageById(url, this.id);
+    this.messageService.getMessage.subscribe(data => this.message = data);
+  }
 
 }
